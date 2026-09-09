205a1f226f23f3e915f3fb42bf272036b1a4f155
Diego Quiñones <diegoqui099@gmail.com>
2026-09-09 16:35:59 +0200

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,121 @@
+# Variables y declaracion de las mismas 
+
+# Nivel principainte 
+# Nombre y apelldio
+
+Nombre = "Diego"
+Apellido = "Quiñones"
+
+print("Me llamo " + Nombre  + " y mi apellido es " + Apellido )
+
+#Punto 2 
+# Crear una variable de cada tipo 
+
+Fruta = "Mango"
+edad = 25
+salario = 1.500
+a = True
+b = False
+
+
+#punto 3 
+# Cambiador de puntos 
+
+Puntos = 10
+
+print("Valor inicial: ", Puntos)
+
+Puntos = 25 
+print("Valor reasignado: ", Puntos)
+
+#punto 4 
+#Guardar datos personales en varibales
+
+NombreEs = "Alexander"
+EdadEs = 26
+Altura = 1.77
+Estudiante = True
+
+print("El nombre del estudiante es: ", NombreEs)
+print("La edad del estudiante es: ", EdadEs)
+print("La altura del estudiante es: ", Altura)
+
+if Estudiante == True:
+    print("Es estudiante")
+else:
+    print("No es estudiante");
+    
+
+## INTERMEDIO ##
+
+#Punto 5
+# Cambiar valor de variable texto y sumarla 
+# Suma de varibales
+
+Q = 10
+W = 9
+Resultado = Q + W
+
+print(Resultado)
+
+
+# Punto 6
+# Conversion de tipos
+
+X = "25"
+
+print(int(X) + 5)
+
+
+# Punto 7
+# Precio de un producto 
+
+NombreP = "mangos"
+precio = 2
+cantidad = 10
+
+total = 10 * 2
+
+print("El valor total de 10 mangos es de: ", total)
+
+
+#Punto 8 
+#Intercambio de valores
+
+D = 5 
+S = 10
+print(D, S)
+
+D, S = S , D
+print(D, S)
+
+
+# Dificil Reto 
+
+# Punto 9
+# Calcura la edad de una persona
+
+nacio = 1999
+Yactual = 2026
+
+calculo = Yactual - nacio
+print("la persona tiene:", calculo , "años")
+
+# Punto 10
+#Listado para videojuego 
+
+nombreG = "Minecraft"
+precioG = 29.99
+jugadores = 1
+disponible = True
+puntuacion = 9.5
+
+print("Nombre:", nombreG)
+print("Precio:", precioG)
+print("Número de jugadores:", jugadores)
+print("Disponible:", disponible)
+print("Puntuación:", puntuacion)
+
+
+
+
